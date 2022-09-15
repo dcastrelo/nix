120e5f4d33acc78461d7cc43ea50abbb739d183c
dcastrelo <castrelo@gmail.com>
2022-09-15 12:31:33 -0300

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-# /bin/sh
-NewFolderLock=castrelo
-NewUser=wdcar
-
-mkdir /var/www/$NewFolderLock/$NewFolderLock
-chown root /var/www/castrelo.com.ar/castrelo.com.ar
-chgrp $NewUser /var/www/castrelo.com.ar/castrelo.com.ar
-chmod 775 /var/www/castrelo.com.ar/castrelo.com.ar
-chmod g+s /var/www/castrelo.com.ar/castrelo.com.ar
