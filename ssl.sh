c33a1f4a14d1afa7dc224adde6ccd043f5e99b02
dcastrelo <castrelo@gmail.com>
2023-05-29 01:58:56 -0300

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -4,3 +4,4 @@ sudo cp /home/dario/ssl23/incaaSSL.ca-bundle /etc/apache2/ssl
 sudo cp /home/dario/ssl23/incaaSSL.crt /etc/apache2/ssl
 sudo cp /home/dario/ssl23/incaaSSL.key /etc/apache2/ssl
 sudo nano /etc/apache2/sites-enabled/*.conf
+sudo service apache2 restart
